6dccc81d0435e9780ca1f2ba051ac6f25220f415
Micah <micahculverhouse1@gmail.com>
2022-07-26 20:50:38 -0600

    file adds and server updates

--- server.js
+++ server.js
@@ -1,32 +1,37 @@
+const path = require('path');
 const express = require("express");
-const sequelize = require("sequelize");
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 const mysql = require("mysql2");
-const handlebars  = require("./views");
 const routes = require("./routes");
+const bcrypt = require('bcrypt');
 const PORT = process.env.port||3001;
 
-const app = express();
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const app = express();
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars")
+
+
+app.use(session(sess));
 app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(routes)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 sequelize.sync({ force: false }).then(() => {
