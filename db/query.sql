aae94ebe766c9180e960acf8c472f9b3e64d47f4
FastEddy1 <edwardrmock@gmail.com>
2022-08-02 22:00:33 -0600

    Create queries to get all users, topics, comments and replies from the db

---
+++ db/query.sql
@@ -0,0 +1,13 @@
+USE breddit_db;
+
+-- Select all users from the users table
+SELECT * FROM users;
+
+-- Select all topics from the topics table
+SELECT * FROM topics;
+
+-- Select all comments from the comments table
+SELECT * FROM comments;
+
+-- Select all replies from the replies table
+SELECT * FROM replies;
