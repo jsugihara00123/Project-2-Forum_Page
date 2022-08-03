52e2754f664b7fa1e690a125ec5815ba4d144af1
FastEddy1 <edwardrmock@gmail.com>
2022-08-02 21:10:39 -0600

    Add missing replies model

---
+++ models/Replies.js
@@ -0,0 +1,46 @@
+const comments = require('../models/comments');
+const replies = require('../models/replies');
+const users = require('../models/user');
+const topics = require('../models/topics');
+const sequelize = require('../config/connection');
+var DataTypes = require('sequelize/lib/data-types');
+
+const repliesPage = sequelize.define('replies', {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    body: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    reply_creation_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.fn ('now'),
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        //foreignKey is from user.id in the database
+        references: {
+            model: 'users',
+            key: 'username',
+        },
+    comment_Id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+    },
+    }, {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'replies',
+    }
+);
+
+    module.exports = repliesPage;
