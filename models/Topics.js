5bcc1742c404f810472b0cf22144ba6b757e8fa8
FastEddy1 <edwardrmock@gmail.com>
2022-08-02 20:44:00 -0600

    Formatting change and switched accounts for proper attribution

--- models/Topics.js
+++ models/Topics.js
@@ -1,41 +1,41 @@
-    const sequelize = require('../config/connection');
-    const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
 
 
-    //Topic model
-    class Topic extends Model {}
-    Topic.init(
-        {
-            id: {
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true
-            },
-            title: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            votes: {
-                type: DataTypes.INTEGER,
-                defaultValue: 0
-            },
-            body: { type: DataTypes.STRING,
-                allowNull: false
-            },
-            topic_creation_date: {
-                type: DataTypes.DATE,
-                defaultValue: sequelize.fn('now')
-            },
-            created_by_user: {
-                type: DataTypes.INTEGER,
-                allowNull: false
-            },
+//Topic model
+class Topic extends Model {}
+Topic.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
         },
-        {   sequelize,
-            freezeTableName: true,
-            underscored: true,
-            modelName: 'topic'
-        }
-    );
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        votes: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        body: { type: DataTypes.STRING,
+            allowNull: false
+        },
+        topic_creation_date: {
+            type: DataTypes.DATE,
+            defaultValue: sequelize.fn('now')
+        },
+        created_by_user: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+    },
+    {   sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'topic'
+    }
+);
 
 module.exports = Topic;
